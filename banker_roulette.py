80bf301e1c7567fe67850a1316b2db3b03f14dae
julianakamura-project <julia.nakamura.work@gmail.com>
2026-08-24 20:39:37 -0300

    Added Banker Roulette

---
+++ banker_roulette.py
@@ -0,0 +1,18 @@
+import random
+
+print("Welcome to the Banker Roulette!\n")
+participants = []
+num_players = int(input("How people will be participating? \n"))
+print("Please input the names of the players.\n")
+for i in range(num_players):
+    participants.append(input(f"Player {i+1}: "))
+print("")
+play = True
+while play:
+    roulette = random.randint(0,len(participants)-1)
+    print(f"The one paying will be {participants[roulette]}!\n")
+    again = input("Would you like to play again? (y/n)\n")
+    if again != "y":
+        print("Thank you for playing!")
+        play = False
+    continue
